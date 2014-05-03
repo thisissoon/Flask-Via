c5caf35b2ddb374744c771834136e17d3de46f3d
Chris Reeves <chris@thisissoon.com>
2014-05-03 19:35:48 +0100

    Fixed tests for flask module renamed to default

--- tests/test_routers/test_default.py
+++ tests/test_routers/test_default.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """
-tests.test_routers.test_flask
-=============================
+tests.test_routers.test_default
+===============================
 
 Unit tests for Flask specific router classes.
 """
@@ -10,7 +10,7 @@ Unit tests for Flask specific router classes.
 import mock
 import unittest
 
-from flask_via.routers import flask
+from flask_via.routers import default
 
 
 class TestFlaskBasicRouter(unittest.TestCase):
@@ -20,7 +20,7 @@ class TestFlaskBasicRouter(unittest.TestCase):
 
     def test_add_to_app(self):
         view = mock.MagicMock()
-        route = flask.Basic('/', view, endpoint='foo')
+        route = default.Basic('/', view, endpoint='foo')
         route.add_to_app(self.app)
 
         self.app.add_url_rule.assert_called_once_with('/', 'foo', view)
@@ -33,7 +33,7 @@ class TestFlaskPluggableRouter(unittest.TestCase):
 
     def test_add_to_app(self):
         view = mock.MagicMock()
-        route = flask.Pluggable('/', view_func=view, endpoint='foo')
+        route = default.Pluggable('/', view_func=view, endpoint='foo')
         route.add_to_app(self.app)
 
         self.app.add_url_rule.assert_called_once_with(
