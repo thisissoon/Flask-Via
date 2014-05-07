4c6f8069c5747cf8f0b34db2ec2d24f4326facce
Chris Reeves <chris@thisissoon.com>
2014-05-07 08:38:48 +0100

    Flask Admin router

---
+++ flask_via/routers/admin.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+"""
+flask_via.routers.admin
+-----------------------
+
+Routers for the Flask-Admin framework.
+"""
+
+from flask_via.routers import BaseRouter
+
+
+class AdminRoute(BaseRouter):
+    """ The Admin router allows you to define Flask-Admin routes and have
+    those views added to the application automatically. For this to
+    work you must at ``init_app`` time pass a optional keyword argument
+    ``flask_admin`` to ``init_app`` with its value being the Flask-Aadmin
+    extension instance.
+
+    Note
+    ----
+    ``Flask-Admin`` has its own way of handling defining urls so this router
+    literally only requires the ``Flask-Admin`` view class.
+
+    Example
+    -------
+    .. sourcecode:: python
+
+        app = Flask(__name__)
+
+        admin = Admin(name='Admin')
+        admin.init_app(app)
+
+        class MyView(BaseView):
+
+            @expose('/')
+            def index(self):
+                return self.render('admin/myindex.html')
+
+        routes = [
+            AdminRoute(MyView(name='Hello'))
+        ]
+
+        via = Via()
+        via.init_app(
+            app,
+            routes_module='flask_via.examples.admin',
+            flask_admin=api)
+
+        if __name__ == '__main__':
+            app.run(debug=True)
+
+    """
+
+    def __init__(self, view):
+        """ Admin route constructor, this router handles adding ``Flask-admin``
+        views to the application.
+
+        Arguments
+        ---------
+        view : flask_admin.base.AdminViewMeta
+            The Flask Admin View Class
+        """
+
+        self.view = view
+
+    def add_to_app(self, app, **kwargs):
+        """ Adds the ``Flask-Admin`` View to the Flask the application.
+
+        Arguments
+        ---------
+        app : flask.app.Flask
+            Flask application instance, this is ignored.
+
+        Keyword Arguments
+        -----------------
+        restful_api : flask_admin.base.Admin
+            Instantiated ``Flask-Admin`` instace, used to add the view.
+
+        Raises
+        ------
+        NotImplementedError
+            If ``flask_admin`` is not provided
+        """
+
+        try:
+            admin = kwargs['flask_admin']
+        except KeyError:
+            raise NotImplementedError(
+                'flask_admin not passed to add_to_app, did you add it to '
+                'via.init_app')
+
+        admin.add_view(self.view)
